6892da118b738d02d9a1d742cf0364c30e998e0e
Salman-Aijaz <salman333699@gmail.com>
2024-10-03 16:20:41 +0500

    database issue fix

--- app/database.py
+++ app/database.py
@@ -8,5 +8,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
-    SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
