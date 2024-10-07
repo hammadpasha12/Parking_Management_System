4d325c1739627d1253b6e9a3cc1f9f230907612b
Salman-Aijaz <salman333699@gmail.com>
2024-10-07 11:53:18 +0500

    make my database and main file synchronous

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
 import os
 from sqlmodel import create_engine, SQLModel,Session
 from dotenv import load_dotenv
+import  logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -8,8 +12,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
-    SQLModel.metadata.create_all(engine)
+    try:
+        SQLModel.metadata.create_all(engine)
+        logger.info("Table created succcessfully")
+    except Exception as e:
+        logger.error(f"Error in initalizing the database {e}") 
+        raise   
 
 def get_db():
-    with Session(engine) as session:
-        yield session
+    try:
+        with Session(engine) as session:
+            yield session
+    except Exception as  e:
+        logger.error(f"Error during databse session {e}") 
+        raise       
--- app/main.py
+++ app/main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 from app.views.parking_view import router
 from app.database import init_db
 from contextlib import asynccontextmanager
+import  logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-@asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()  
+    try:
+        init_db()  
+        logger.info("Database Initialized Successfully")
+    except Exception as e:
+        logger.error(f"Failed to initialized the database {e}")
+        raise
     yield
+
+        
 
 app: FastAPI = FastAPI(lifespan=lifespan)
 app.include_router(router)
